ac0661f5802bfe9495ffa1c0ea0a2ba41e73bd27
sjaras <sjara@mined.academy>
2024-05-13 20:25:01 -0500

    fix: remove grid style

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Card({ icon, time, text }: { icon: string, time: string, text: string }) {
     return (
-        <div className='h-full flex flex-col bg-slate-300  justify-center'>
+        <div className='h-full flex flex-1 flex-col bg-slate-300  justify-center'>
             <div>{icon}{time}</div>
             <p className='overflow-x-hidden line-clamp-1'>
                 {text}
--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -9,7 +9,7 @@ interface CardProps {
 
 export default function CardContainer({ items }: { items: CardProps[] }) {
     return (
-        <div className='h-[170] min-h-[170px] max-h-[170px] w-[150px] bg-slate-400 border rounded-lg p-1 overflow-y-hidden grid gap-[3px] '>
+        <div className='h-[170] min-h-[170px] max-h-[170px] w-[150px] bg-slate-400 border rounded-lg p-1 overflow-y-hidden flex flex-col gap-[3px] '>
             {
                 items.map((card, index) => (
                     <Card key={index} icon='♥' time='11:13' text='Aprende Forex con '></Card>
